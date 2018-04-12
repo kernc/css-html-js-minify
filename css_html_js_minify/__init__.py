75e185581ac0d6f178ef30cfead565dceab7832e
Juan Carlos <juan@thirstie.com>
2018-04-12 16:17:47 -0300

    Refactor.

--- css_html_js_minify/__init__.py
+++ css_html_js_minify/__init__.py
@@ -13,17 +13,7 @@ from .minify import (process_single_html_file, process_single_js_file,
                      css_minify)
 
 
-__version__ = '2.5.0'
-__license__ = 'GPLv3+ LGPLv3+'
-__author__ = 'Juan Carlos'
-__email__ = 'juancarlospaco@gmail.com'
-__url__ = 'https://github.com/juancarlospaco/css-html-js-minify'
-__source__ = ('https://raw.githubusercontent.com/juancarlospaco/'
-              'css-html-js-minify/master/css-html-js-minify.py')
-
-
-__all__ = ['__version__', '__license__', '__author__',
-           '__email__', '__url__', '__source__',
-           'process_single_html_file', 'process_single_js_file',
-           'process_single_css_file', 'html_minify', 'js_minify',
-           'css_minify', 'minify']
+__version__ = '2.5.5'
+__all__ = ('__version__', 'process_single_html_file', 'process_single_js_file',
+           'process_single_css_file', 'html_minify', 'js_minify', 'css_minify',
+           'minify')
