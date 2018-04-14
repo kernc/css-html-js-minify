4e2458431e611f12062cb275ca57161d3c600808
Juan Carlos <juan@thirstie.com>
2018-04-14 10:32:00 -0300

    Fix log

--- css_html_js_minify/html_minifier.py
+++ css_html_js_minify/html_minifier.py
@@ -7,8 +7,6 @@
 
 import re
 
-import logging as log
-
 
 __all__ = ('html_minify', )
 
@@ -19,7 +17,6 @@ def condense_html_whitespace(html):
     >>> condense_html_whitespace('<i>  <b>    <a> test </a>    </b> </i><br>')
     '<i><b><a> test </a></b></i><br>'
     """  # first space between tags, then empty new lines and in-between.
-    log.debug("Removing unnecessary HTML White Spaces and Empty New Lines.")
     tagsStack = []
     split = re.split('(<\\s*pre.*>|<\\s*/\\s*pre\\s*>|<\\s*textarea.*>|<\\s*/\\s*textarea\\s*>)', html, flags=re.IGNORECASE)
     for i in range(0, len(split)):
@@ -56,7 +53,6 @@ def condense_style(html):
     >>> condense_style('<style type="text/css">*{border:0}</style><p>a b c')
     '<style>*{border:0}</style><p>a b c'
     """  # May look silly but Emmet does this and is wrong.
-    log.debug("Condensing HTML Style CSS tags.")
     return html.replace('<style type="text/css">', '<style>').replace(
         "<style type='text/css'>", '<style>').replace(
             "<style type=text/css>", '<style>')
@@ -68,7 +64,6 @@ def condense_script(html):
     >>> condense_script('<script type="text/javascript"> </script><p>a b c')
     '<script> </script><p>a b c'
     """  # May look silly but Emmet does this and is wrong.
-    log.debug("Condensing HTML Script JS tags.")
     return html.replace('<script type="text/javascript">', '<script>').replace(
         "<style type='text/javascript'>", '<script>').replace(
             "<style type=text/javascript>", '<script>')
@@ -80,7 +75,6 @@ def clean_unneeded_html_tags(html):
     >>> clean_unneeded_html_tags('a<body></img></td>b</th></tr></hr></br>c')
     'abc'
     """
-    log.debug("Removing unnecessary optional HTML tags.")
     for tag_to_remove in ("""</area> </base> <body> </body> </br> </col>
         </colgroup> </dd> </dt> <head> </head> </hr> <html> </html> </img>
         </input> </li> </link> </meta> </option> </param> <tbody> </tbody>
@@ -97,7 +91,6 @@ def remove_html_comments(html):
     >>> _+= "<!-- kill me please -->keep" ; remove_html_comments(_)
     '<!-- build:dev -->a<!-- endbuild -->b<!--[if IE 7]>c<![endif]--> keep'
     """  # Grunt uses comments to as build arguments, bad practice but still.
-    log.debug("""Removing all unnecessary HTML comments.""")
     return re.compile(r'<!-- .*? -->', re.I).sub('', html)
 
 
@@ -107,7 +100,6 @@ def unquote_html_attributes(html):
     >>> unquote_html_attributes('<img   width="9" height="5" data-foo="0"  >')
     '<img width=9 height=5 data-foo=0 >'
     """  # data-foo=0> might cause errors on IE, we leave 1 space data-foo=0 >
-    log.debug("Removing unnecessary Quotes on attributes of HTML tags.")
     # cache all regular expressions on variables before we enter the for loop.
     any_tag = re.compile(r"<\w.*?>", re.I | re.MULTILINE | re.DOTALL)
     space = re.compile(r' \s+|\s +', re.MULTILINE)
@@ -144,12 +136,10 @@ def html_minify(html, comments=False):
     >>> html_minify(' <p  width="9" height="5"  > <!-- a --> b </p> c <br> ')
     '<p width=9 height=5 > b c <br>'
     """
-    log.info("Compressing HTML...")
     html = remove_html_comments(html) if not comments else html
     html = condense_style(html)
     html = condense_script(html)
     html = clean_unneeded_html_tags(html)
     html = condense_html_whitespace(html)
     html = unquote_html_attributes(html)
-    log.info("Finished compressing HTML !.")
     return html.strip()
